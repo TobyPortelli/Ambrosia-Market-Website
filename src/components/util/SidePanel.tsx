f2787543cfdc6bb1044ce5d9a8c97de61eee8a79
amp7368 <47309189+amp7368@users.noreply.github.com>
2021-12-31 22:37:37 -0500

    adding some sass

--- src/components/util/SidePanel.tsx
+++ src/components/util/SidePanel.tsx
@@ -1,3 +1,4 @@
+import "./SidePanelSS.scss";
 import { AmbrosiaMarketIcon } from "../ImageHandling";
 
 class rowInfo {
@@ -16,36 +17,54 @@ class rowInfo {
 
 class SidePanel {
   static createSidePanel(): JSX.Element {
-    let titleRow = new rowInfo(
+    let titleRowInfo = new rowInfo(
       AmbrosiaMarketIcon,
       "Ambrosia Market"
     );
+    let titleRow = SidePanel.createIconRow(
+      titleRowInfo.createIcon(),
+      SidePanel.createLoginRow(titleRowInfo)
+    );
+    let loginRow = SidePanel.createLoginRow(titleRowInfo);
     return <>
       <div className={"side_panel_root_container"}>
         <div className={"side_panel_root"}>
-          {SidePanel.createTitleRow(titleRow)}
+          {SidePanel.createRow(titleRow)}
+          {SidePanel.createRow(loginRow)}
         </div>
       </div>
     </>;
   }
 
-  private static createRow() {
-
-  }
-
-  private static createTitleRow(rowInfo1: rowInfo) {
+  private static createRow(inner: JSX.Element): JSX.Element {
     return <>
       <div className={"side_panel_row_container"}>
+        {inner}
+      </div>
+    </>;
+  }
 
+  private static createIconRow(icon: JSX.Element, content: JSX.Element) {
+    return <>
+      <div className={"side_panel_row_icon_container"}>
         <div className={"side_panel_row_icon"}>
-          {rowInfo1.createIcon()}
-        </div>
-        <div className={"side_panel_row_content"}>
-
+          {icon}
         </div>
       </div>
-    </>
-      ;
+      <div className={"side_panel_row_content_container"}>
+        <div className={"side_panel_row_content"}>
+          {content}
+        </div>
+      </div>
+    </>;
+  }
+
+  private static createLoginRow(titleRow: rowInfo) {
+    return <>
+      <text>
+        {titleRow.text}
+      </text>
+    </>;
   }
 }
 
